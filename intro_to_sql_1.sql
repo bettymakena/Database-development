99c78b0ed88cc88d9041a403ae6b204b53b0bc30
bettymakena <52485487+bettymakena@users.noreply.github.com>
2021-03-15 22:27:13 +0300

    Add files via upload

---
+++ intro_to_sql_1.sql
@@ -0,0 +1,29 @@
+/*CREATE DATABASE sunday17th;*/
+/*CREATE SCHEMA cho1;*/
+
+/*CREATE TABLE students(
+id char(4) PRIMARY KEY, fname varchar(10),
+lname varchar(14), depart varchar (10),
+year int
+)*/
+
+/*INSERT INTO students(id, fname, lname, depart,year)
+VALUES
+(1234, 'Risper', 'Kendi', 'Electrical', 1),(2351, 'Aura', 'Perti','Acturial', 4),
+(7865, 'Antony', 'Kinga', 'Iggress', 5), (6784, 'Faith', 'Kendi', 'Igress', 4),
+(6720, 'Anne', 'Kito', 'Bcom', 3), (9846, 'Brian', 'Musa', 'Acturial', 1),
+(3458, 'Benjamin', 'Muni', 'Acturial', 4), (5673, 'Agnes', 'Meti', 'Electrial', 1),
+(5674, 'Rono','Chepkuri','Iggress', 4), (4678, 'Sly', 'Karimi', 'Igress', 3),
+(6729, 'Eva', 'Ngamia', 'Bcom', 2), (4562, 'Emma', 'Moni', 'BA', 3),
+(2355, 'Nana', 'Halali', 'Acturial', 4), (7623, 'Peta', 'Bure', 'Bcom', 4);*/
+
+/*see the  in your table 
+SELECT*
+FROM students */
+
+
+
+
+
+
+
